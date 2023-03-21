acc8c559f8a95af1dd0eb5c4be8a7cd6c65aa324
zamanabdi <zaman.abdi24@gmail.com>
2023-03-21 19:44:45 +0530

    setup the HomeHeader and logo inside home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,17 @@ import HomeHeader from "./HomeHeader";
 import SearchInput from "./SearchInput";
 import Footer from "./Footer";
 const Home = () => {
-    return <div>Home</div>;
+
+    return (
+        <div className="flex h-[100vh] flex-col">
+        <HomeHeader/>
+        <main className="grow flex justify-center">
+        <div className="w-full px-5 flex flex-col items-center mt-44">
+        <img src={Logo} className="w-[172px] md:w-[272px] mb-8"/>
+        </div>
+        </main>
+        </div>
+    )
 };
 
 export default Home;
