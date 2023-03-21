c889048a748da67528665d801ff2bac82f4f1cde
zamanabdi <zaman.abdi24@gmail.com>
2023-03-21 21:09:55 +0530

    create the header section and the logo and profile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,8 +10,17 @@ const Home = () => {
         <main className="grow flex justify-center">
         <div className="w-full px-5 flex flex-col items-center mt-44">
         <img src={Logo} className="w-[172px] md:w-[272px] mb-8"/>
+        <SearchInput/>
+        <div className="flex gap-2 text-[#3c4043] mt-8">
+
+        <button className="h-9 px-4 bg-[#f8f9fa] text-sm rounded-md border border-[#f8f9fa] hover:border-[#dadce0] hover:shadow-c2">Google Search</button>
+
+        <button className="h-9 px-4 bg-[#f8f9fa] text-sm rounded-md border border-[#f8f9fa] hover:border-[#dadce0] hover:shadow-c2">I'm Feeling Lucky</button>
+
+        </div>
         </div>
         </main>
+        <Footer/>
         </div>
     )
 };
